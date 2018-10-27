64429936d6f8896eb6785e049fc957e6314b7442
Zadey <41181506+Zadezter@users.noreply.github.com>
2018-10-27 21:32:26 +0800

    Update mythcord.js

--- mythcord.js
+++ mythcord.js
@@ -21,6 +21,8 @@ const crypto = require('crypto');
 
 const files = require("fs");
 
+var reactions = ['🤔','😱','👀','👌','🌀'];
+
 var firstRun = 1;
 var readySpam = 0;
 
@@ -43,44 +45,61 @@ client.on('message', message => {
     if (message.author.bot) return;
     if (string(message.content).startsWith(config.prefix)) {
 
-      var command = message.content.split(" ")[0];
-      command = string(command).chompLeft(config.prefix).s.toLowerCase();
-      console.log('An command ' + command + ' has been received from ' + message.author.username + ' in Discord app.');
-      if (string(message.content).startsWith(config.prefix)) {
+        var command = message.content.split(" ")[0];
+        command = string(command).chompLeft(config.prefix).s.toLowerCase();
+        console.log('An command ' + command + ' has been received from ' + message.author.username + ' in Discord app.');
+        if (string(message.content).startsWith(config.prefix)) {
+        if (message.guild.id == config.mainGuild) return;
       
-      var arguments = message.content.split(" ").slice(1);
-      var commandComplete = true;
-      switch (command) {
-        case 'status':
-        var seconds = process.uptime();
+        var arguments = message.content.split(" ").slice(1);
+        var commandComplete = true;
+        switch (command) {
+          case 'status':
+          var seconds = process.uptime();
               
-        days = Math.floor(seconds / 86400);
-        seconds %= 86400;
+          days = Math.floor(seconds / 86400);
+          seconds %= 86400;
               
-        hours = Math.floor(seconds / 3600);
-        seconds %= 3600;
+          hours = Math.floor(seconds / 3600);
+          seconds %= 3600;
               
-        minutes = Math.floor(seconds / 60);
-        seconds = seconds % 60;
+          minutes = Math.floor(seconds / 60);
+          isSeconds = seconds % 60;
               
-        var uptime = days + ' days ' + hours + ' hours ' + minutes + ' minutes ' + Math.round(seconds) + ' seconds';
-        var stats = new library.SubFields()
-          .addField('Total Servers', client.guilds.size)
-          .addField('Total Channels', client.channels.size)
-          .addField('Cached Users', client.users.size)
-          .addField('')
-          .addField('Uptime', uptime)
-          .addField('Memory RAM Usage', Math.round (process.memoryUsage().rss / 10485.76) / 100 + ' MB')
-          .toString();
-        var status = new Discord.RichEmbed()
-          .addField('Stats:', stats);
-        sendEmbed(message.channel, status);
-        break;
+          var uptime = days + ' days ' + hours + ' hours ' + minutes + ' minutes ' + Math.round(isSeconds) + ' seconds';
+          var stats = new library.SubFields()
+            .addField('Total Servers', client.guilds.size)
+            .addField('Total Channels', client.channels.size)
+            .addField('Cached Users', client.users.size)
+            .addField('')
+            .addField('Uptime', uptime)
+            .addField('Memory RAM Usage', Math.round(process.memoryUsage().rss / 10485.76) / 100 + ' MB')
+            .toString();
+          var status = new Discord.RichEmbed()
+            .addField('Stats:', stats);
+          sendEmbed(message.channel, status);
+          break;
         
-        case '8ball':
+          case '8ball':
+          var random = Math.floor(Math.random() * config.eightBall.length);
+          message.reply(config.eightBall[random]);
+          break;
+                
+          case 'help':
+          message.reply('Sent you a DM!');
+          var help = new Discord.RichEmbed()
+            .addField('/help', 'Generate some available commands in DM user.')
+            .addField('/8ball', 'Ask the magic 8Ball for a questions/answers.')
+            .addField('/status', 'Display the statstics about this bot.')
+          message.author.send("", {embed: help});
+         break;
+       default:
+        if (message.guild.id == config.mainGuild) {
+          commandComplete = false;
+        }
         break;
       }
-    }
+   }
 });
 
 function sendEmbed(channel, embed) {

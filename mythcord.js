1c0a8095c779356527f333614bf43ab87a17cc5d
Zadey <41181506+Zadezter@users.noreply.github.com>
2018-10-31 03:59:55 +0000

    Update mythcord.js

--- mythcord.js
+++ mythcord.js
@@ -76,8 +76,8 @@ client.on('message', async message => {
               return message.reply('Hey, no mentioned user detected. This cannot work.');
             }
             var gayRate = new Discord.RichEmbed()
-            .addField('^ ^ ^', '__' + Math.floor(Math.random() * 101) + '__/**100**% Gay! :gay_pride_flag:')
-            .setFooter(`To ${message.author.username}`, `${message.author.avatarURL}`);
+            .addField('${message.member.username}', '__' + Math.floor(Math.random() * 101) + '__/**100**% Gay! :gay_pride_flag:')
+            .setFooter(`To ${message.member.username}`, `${message.member.avatarURL}`);
             sendEmbed(message.channel, gayRate);
             break;
                   
